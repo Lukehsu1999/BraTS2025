3c1dc97c2da7788e3ff331dfa1eda3fff00fa1c2
Lukehsu1999 <lukehsu1999@gmail.com>
2025-06-06 16:31:56 +0800

    split_data: update the dropout logic, fix path, set determinism

---
+++ split_data.py
@@ -0,0 +1,48 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+import os
+import glob
+
+def is_train(row, dropout, train, val, test):
+    if row in dropout:
+        return 'Dropout'
+    elif row in train:
+        return 'Train'
+    elif row in val:
+        return 'Validation'
+    else:
+        return 'Test'
+
+# Train - Val - Test Ratio: 0.64: 0.16: 0.20
+# data_usage_ratio: how many percentage of data we use, and how many we discard
+def make_split_tag(patient_list, data_usage_ratio=1, seed=0):
+    patient_id = [os.path.basename(i) for i in patient_list]
+    
+    # Fix test_size -> train_size here
+    dropout_patient, train_patient = train_test_split(patient_id,train_size=data_usage_ratio, random_state=seed)
+    train_patient, test_patient = train_test_split(train_patient,test_size=0.2, random_state=seed)
+    train_patient, val_patient = train_test_split(train_patient,test_size=0.2, random_state=seed)
+    data_split = [ is_train(pid, dropout_patient, train_patient, val_patient, test_patient) for pid in patient_id ]
+    meta = []
+    for p, d in zip(patient_id, data_split):
+        meta.append({
+            "folder_name": p,
+            "data_split": d
+        })
+    
+    return meta
+
+if __name__ == '__main__':
+    data_dir = "/media/volume1/BraTS2025/7/MICCAI2024-BraTS-GoAT-TrainingData-With-GroundTruth/MICCAI2024-BraTS-GoAT-TrainingData-With-GroundTruth"
+    
+    meta_path = './meta'
+    os.makedirs(meta_path, exist_ok=True)
+    
+    # set determinism to ensure reproducibility
+    seed = 0 
+    patient_list = os.listdir(data_dir)
+    meta = make_split_tag(patient_list, data_usage_ratio=1, seed=seed)
+    
+    df = pd.DataFrame(meta)
+    df.to_csv('./meta/data_split_UsageFull.csv', index=False)
